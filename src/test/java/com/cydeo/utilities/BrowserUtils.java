d40417ab9c9b4800b8857fe4784b4c86511f5315
01ihsan <karatasihsan01@gmail.com>
2022-06-13 11:58:36 +0200

    BigBang

---
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -0,0 +1,15 @@
+package com.cydeo.utilities;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class BrowserUtils {
+    public static void waitForInvisibilityOf(WebElement webElement){
+        Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        wait.until(ExpectedConditions.invisibilityOf(webElement));
+    }
+}
