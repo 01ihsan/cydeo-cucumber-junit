2b1682e01b162e0df8928c432b592bc2829f7474
01ihsan <karatasihsan01@gmail.com>
2022-06-15 12:36:47 +0200

    webTableLogin created

---
+++ src/test/java/com/cydeo/step_definitions/WebTableStepDefinitions.java
@@ -0,0 +1,32 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class WebTableStepDefinitions {
+
+    @Given("user is on the login page of web table app")
+    public void userIsOnTheLoginPageOfWebTableApp() {
+    }
+    @When("user enters username {string}")
+    public void userEntersUsername(String arg0) {
+    }
+
+    @And("user enters password {string}")
+    public void userEntersPassword(String arg0) {
+
+    }
+
+    @And("user clicks to login button")
+    public void userClicksToLoginButton() {
+
+    }
+
+    @Then("user should see url contains orders")
+    public void userShouldSeeUrlContainsOrders() {
+
+    }
+
+}
