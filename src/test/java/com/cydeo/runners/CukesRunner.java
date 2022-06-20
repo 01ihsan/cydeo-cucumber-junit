98cb5ef4f6e580ff1d03f110cb8df34e0e6ecdc0
01ihsan <karatasihsan01@gmail.com>
2022-06-20 13:16:24 +0200

    FailedTestRunner created to reRun failed tests.

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -6,7 +6,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "html:target/cucumber-report.html",
+        plugin = {
+                "html:target/cucumber-report.html",
+                "rerun:target/reRun.txt"
+        },
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false, //turns of the step_definitions. It will get only snippets.
