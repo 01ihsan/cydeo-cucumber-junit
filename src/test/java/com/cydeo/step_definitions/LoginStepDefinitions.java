7880f8b93c79d052711c7d51b5d3a91fd179d29c
01ihsan <karatasihsan01@gmail.com>
2022-06-21 16:44:28 +0200

    @update

--- src/test/java/com/cydeo/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/LoginStepDefinitions.java
@@ -10,38 +10,30 @@ public class LoginStepDefinitions {
     public void userEntersLibrarianUsername() {
         System.out.println("user enters librarian username");
     }
-
     @And("user enters librarian password")
     public void userEntersLibrarianPassword() {
         System.out.println("user enters librarian password");
     }
-
     @Then("user should see the dashboard")
     public void userShouldSeeTheDashboard() {
         System.out.println("user should see the dashboard");
     }
-
     @When("user enters student username")
     public void userEntersStudentUsername() {
         System.out.println("user enters student username");
     }
-
     @And("user enters student password")
     public void userEntersStudentPassword() {
         System.out.println("user enters student password");
     }
-
     @When("user enters admin username")
     public void userEntersAdminUsername() {
         System.out.println("user enters admin username");
-
     }
-
     @And("user enters admin password")
     public void userEntersAdminPassword() {
         System.out.println("user enters admin password");
     }
-
     @Given("user is on the library log in page")
     public void userIsOnTheLibraryLogInPage() {
         System.out.println("user on the log in page");
