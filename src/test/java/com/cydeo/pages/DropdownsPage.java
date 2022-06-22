2eee891293593c09c36b10ae841e9124ddd778eb
01ihsan <karatasihsan01@gmail.com>
2022-06-22 17:50:31 +0200

    update

--- src/test/java/com/cydeo/pages/DropdownsPage.java
+++ src/test/java/com/cydeo/pages/DropdownsPage.java
@@ -6,9 +6,10 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DropdownsPage {
-    public DropdownsPage(){
-        PageFactory.initElements(Driver.getDriver(),this);
+    public DropdownsPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
     }
+
     @FindBy(id = "month")
     public WebElement monthDropdown;
 }
